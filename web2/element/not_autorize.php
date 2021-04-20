7c1cc8b8841d20b5910d1d546be80ad7f93f4476
Mastylina <72931346+Mastylina@users.noreply.github.com>
2021-04-20 19:03:25 +0300

    Add files via upload

---
+++ web2/element/not_autorize.php
@@ -0,0 +1,18 @@
+<?php
+include_once('./header.html');
+require_once('setTable.php');
+?>
+<div class="container-xxl">
+    <a class="btn btn-primary" href="registration.php" role="button">Регистрация</a>
+    <a class="btn btn-primary" href="input.php" role="button">Вход</a>
+</div>
+<div class="card" style="width: 40rem; margin:0 auto;">
+    <img src="konfer.jpg" class="card-img-top">
+    <div class="card-body">
+        <p class="card-text">Сайт предназначен для регистрации докладчиков на конференции.</p>
+    </div>
+</div>
+
+   
+
+   
